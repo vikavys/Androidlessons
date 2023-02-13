235487185475476a8e54b1e7b431112810108539
Kristina <kristinasleptcova89@mail.ru>
2023-02-13 14:56:57 +0900

    проект Android

---
+++ p0051layoutfiles/src/main/java/com/example/p0051layoutfiles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p0051layoutfiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
